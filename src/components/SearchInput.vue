d04a0323648d1b5427cad9ba5725864b12dc2ff9
Maks-Jk <sinplym1@gmail.com>
2024-04-06 13:57:48 +0500

    feat: create basic SearchInput component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <input type="text" v-model="query" @input="handleInput" placeholder="Search...">
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue';
+
+const props = defineProps<{
+  handleSearch: (query: string) => void;
+}>()
+
+const query = ref('');
+
+const handleInput = () => {
+  props.handleSearch(query.value.trim());
+}
+</script>
+
+<style>
+</style>
