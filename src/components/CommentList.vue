f7fc38bf44947930466ce8ac709565d05e6fe28e
Maks-Jk <sinplym1@gmail.com>
2024-04-06 14:14:37 +0500

    feat: create basic CommentList component

---
+++ src/components/CommentList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="comment in comments" :key="comment.id">
+      <div>
+        <strong>{{ comment.name }}</strong>
+        <p>{{ comment.body }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+  body: string;
+}
+
+const comments = ref<Comment[]>([]);
+
+// Принимаем свойство comments из вне
+const { props } = defineProps(['comments']);
+</script>
