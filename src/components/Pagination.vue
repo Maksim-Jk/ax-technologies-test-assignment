565414805abb02f8d1c8f98c30ec63b8d412f05c
Maks-Jk <sinplym1@gmail.com>
2024-04-06 13:57:21 +0500

    feat: create basic pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="goToPage(currentPage - 1)" :disabled="currentPage === 1">Prev</button>
+    <span>Page {{ currentPage }} of {{ totalPages }}</span>
+    <button @click="goToPage(currentPage + 1)" :disabled="currentPage === totalPages">Next</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+  totalPages: number;
+  currentPage: number;
+  handlePageChange: (page: number) => void
+}>()
+
+const goToPage = (page: number) => {
+  if (page >= 1 && page <= props.totalPages) {
+    props.handlePageChange(page);
+  }
+}
+</script>
+
+<style>
+</style>
