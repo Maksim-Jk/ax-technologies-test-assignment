fe211615302b0f7696f5431e77a643ad2a397153
Maks-Jk <sinplym1@gmail.com>
2024-04-12 18:02:33 +0500

    fix: typescript issue

--- src/components/ui/SearchInput.vue
+++ src/components/ui/SearchInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="search-input-container">
     <input type="text" v-model="searchQuery" placeholder="Search..." class="search-input">
-    <div v-if="searchQuery" @click="handleReset" class="reset-button">
+    <div v-if="query" @click="handleReset" class="reset-button">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
         <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
       </svg>
@@ -10,29 +10,26 @@
 </template>
 
 <script setup lang="ts">
-import {computed, ref} from 'vue';
+import {computed, ref, defineEmits} from 'vue';
 import {debounce} from '@/utils/debounce.ts';
 
 const query = ref('');
-
 const emits = defineEmits(['input', 'reset']);
 
-const handleInput = () => {
-  emits('input', query.value.trim());
-};
+const searchQuery = computed({
+  get: () => {
+    return query.value;
+  },
+  set: (value: string) => {
+    query.value = value;
+    debounce(() => emits('input', query.value.trim()), 300)();
+  }
+});
 
 const handleReset = () => {
   query.value = '';
   emits('input', '');
 }
-
-const searchQuery = computed({
-  get: () => query.value,
-  set: debounce((value: string) => {
-    query.value = value;
-    handleInput();
-  }, 300)
-});
 </script>
 
 <style scoped>
